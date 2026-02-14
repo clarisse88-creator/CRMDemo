3ceafb10f1c2ca3cbd193eec26c14251c7814d24
Clarisse88-creator <nyiraminaniclarisse88@gmail.com>
2026-02-14 21:55:09 +0200

    vv

--- Core/Application/obj/Debug/net10.0/Application.AssemblyInfo.cs
+++ Core/Application/obj/Debug/net10.0/Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e3a700f38ba5dd8fe2aca61b6cddd0fbcf5c702")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+361095f49afecd1017416bba830dc94b3da7ed1f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
