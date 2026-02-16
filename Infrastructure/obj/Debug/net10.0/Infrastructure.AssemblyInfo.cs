a05d98d0adcd53bc40ede9b87a96aa01ad9ce111
Clarisse88-creator <nyiraminaniclarisse88@gmail.com>
2026-02-16 10:18:46 +0200

    Monday

--- Infrastructure/obj/Debug/net10.0/Infrastructure.AssemblyInfo.cs
+++ Infrastructure/obj/Debug/net10.0/Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55efd7b3e8a327ad1505ab6bd4dbae5eca1a3dc7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50fce5bb99245e8a69d77469b9569007796e48df")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
