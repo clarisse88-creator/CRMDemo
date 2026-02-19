88b25b3c635e518437e470058eece9278ca85c42
Clarisse88-creator <nyiraminaniclarisse88@gmail.com>
2026-02-19 09:35:05 +0200

    fin

--- Web/obj/Debug/net10.0/Web.AssemblyInfo.cs
+++ Web/obj/Debug/net10.0/Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+969b452d795ef8f3e8c2e5cddd066400f86ef71e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+407af9bfe64d2962febdfa0482f1eea456fbf53e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
