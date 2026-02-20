5e94ccd8d954e0142fb031e8bb48e13d7543c76f
Clarisse88-creator <nyiraminaniclarisse88@gmail.com>
2026-02-20 14:21:26 +0200

    hgh

--- Core/Application/obj/Debug/net10.0/Application.AssemblyInfo.cs
+++ Core/Application/obj/Debug/net10.0/Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88b25b3c635e518437e470058eece9278ca85c42")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d3d5b53260c36fd905e5cd2a7dbd45ed95d1751")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Web/obj/Debug/net10.0/Web.AssemblyInfo.cs
+++ Web/obj/Debug/net10.0/Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88b25b3c635e518437e470058eece9278ca85c42")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d3d5b53260c36fd905e5cd2a7dbd45ed95d1751")]
 [assembly: System.Reflection.AssemblyProductAttribute("Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
