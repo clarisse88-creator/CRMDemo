5b5e45886f5c7dc3c0e7e5e6503573b7cb64e106
Clarisse88-creator <nyiraminaniclarisse88@gmail.com>
2026-02-20 19:25:46 +0200

    fin

--- Core/Domain/obj/Debug/net10.0/Domain.AssemblyInfo.cs
+++ Core/Domain/obj/Debug/net10.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e94ccd8d954e0142fb031e8bb48e13d7543c76f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae56f3da605bc115ca9f56c1ecdec90db1e7c4d4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
