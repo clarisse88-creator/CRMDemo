b4821701280eb373b6610bec849511261c904980
Clarisse88-creator <nyiraminaniclarisse88@gmail.com>
2026-03-03 15:12:53 +0200

    bv

--- Infrastructure/obj/Debug/net10.0/Infrastructure.AssemblyInfo.cs
+++ Infrastructure/obj/Debug/net10.0/Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca463ca3d1fe3d4499e3fd6dbb6f7c427629a4a0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+061366334193daf22112dc39cba2d55b0c304364")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
